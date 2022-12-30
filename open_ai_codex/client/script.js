d610ef685dbe43589b71bf47ff99681777e6843d
Monique Shaqiri <moniqueshaqiri@Moniques-MacBook-Air.fritz.box>
2022-12-30 13:24:19 +0100

    finished client

--- open_ai_codex/client/script.js
+++ open_ai_codex/client/script.js
@@ -61,3 +61,17 @@ function chatStripe(isAi, value, uniqueId) {
     `
     )
 }
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+  //user`s chatStripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+  form.reset();
+  //bot chatStripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+  const messageDiv = document.getElementById(uniqueId);
+  loader(messageDiv);
+
+}
