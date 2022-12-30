abee5e3db9165cebb6fbbaa4afd9f6eb08f1ea11
Monique Shaqiri <ms-digitalsolution@gmx.ch>
2022-12-30 16:21:53 +0100

    Finished

--- open_ai_codex/client/script.js
+++ open_ai_codex/client/script.js
@@ -61,42 +61,60 @@ function chatStripe(isAi, value, uniqueId) {
     `
     )
 }
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  const data = new FormData(form);
-  //user`s chatStripe
-  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
-  form.reset();
-  //bot chatStripe
-  const uniqueId = generateUniqueId();
-  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
-  chatContainer.scrollTop = chatContainer.scrollHeight;
-  const messageDiv = document.getElementById(uniqueId);
-  loader(messageDiv);
-  // fetch data from server
-  const response = await fetch('http://localhost:5002,',{
-    method: 'POST',
-    headers:{
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        prompt: data.get('prompt')
-    })
-  })
-  clearInterval(loadInterval);
-  messageDiv.innerHTML = "";
-  
-  if(response.ok){
-    const data = await response.json();
-    const parseData = data.bot.trim();
-    typeText(messageDiv,parseData);
-  }
-  
 
+const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const data = new FormData(form)
+
+    // user's chatstripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+    // to clear the textarea input 
+    form.reset()
+
+    // bot's chatstripe
+    const uniqueId = generateUniqueId()
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+
+    // to focus scroll to the bottom 
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    // specific message div 
+    const messageDiv = document.getElementById(uniqueId)
+
+    // messageDiv.innerHTML = "..."
+    loader(messageDiv)
+
+    const response = await fetch('https://codex-im0y.onrender.com/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
+    })
+
+    clearInterval(loadInterval)
+    messageDiv.innerHTML = " "
+
+    if (response.ok) {
+        const data = await response.json();
+        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+
+        typeText(messageDiv, parsedData)
+    } else {
+        const err = await response.text()
+
+        messageDiv.innerHTML = "Something went wrong"
+        alert(err)
+    }
 }
-form.addEventListener('submit',handleSubmit);
-form.addEventListener('keyup', (e) =>{
-  if(e.keyCode === 13){
-    handleSubmit(e);
-  }
+
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        handleSubmit(e)
+    }
 })
